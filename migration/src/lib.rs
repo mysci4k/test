f6310ed32324bbd88b00acfe0165a29291b4727b
Adam Szamalek <szamalekadam@gmail.com>
2025-11-02 21:15:34 +0100

    feat: add board table migration

--- migration/src/lib.rs
+++ migration/src/lib.rs
@@ -1,4 +1,5 @@
 mod m20251102_200527_create_user_table;
+mod m20251102_201124_create_board_table;
 
 pub use sea_orm_migration::prelude::*;
 
@@ -7,6 +8,9 @@ pub struct Migrator;
 #[async_trait::async_trait]
 impl MigratorTrait for Migrator {
     fn migrations() -> Vec<Box<dyn MigrationTrait>> {
-        vec![Box::new(m20251102_200527_create_user_table::Migration)]
+        vec![
+            Box::new(m20251102_200527_create_user_table::Migration),
+            Box::new(m20251102_201124_create_board_table::Migration),
+        ]
     }
 }
