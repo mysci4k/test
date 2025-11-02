2cf5be98200a376adcf9b7048f72c63556a3beb8
Adam Szamalek <szamalekadam@gmail.com>
2025-11-02 19:47:55 +0100

    feat: load .env file at startup

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 mod presentation;
 
 use crate::presentation::http::server::configure_server;
+use dotenvy::dotenv;
 use std::io::Result;
 
 #[actix_web::main]
 async fn main() -> Result<()> {
+    dotenv().ok();
+
     let server = configure_server("127.0.0.1", 8080).await?;
 
     server.await
