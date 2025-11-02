d73a5782b75dc1bc35c8abc0c1bd809b5a9d170d
Adam Szamalek <szamalekadam@gmail.com>
2025-11-02 19:55:33 +0100

    feat: initialize tracing subscriber for logging

--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,18 @@ use crate::{
 };
 use dotenvy::dotenv;
 use std::io::Result;
+use tracing::Level;
+use tracing_subscriber::FmtSubscriber;
 
 #[actix_web::main]
 async fn main() -> Result<()> {
     dotenv().ok();
+
+    let subscriber = FmtSubscriber::builder()
+        .with_max_level(Level::INFO)
+        .finish();
+    tracing::subscriber::set_global_default(subscriber)
+        .expect("Failed to set global default subscriber");
 
     let server = configure_server(&SERVER_ADDRESS, *SERVER_PORT).await?;
 
