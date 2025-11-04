0a33a02a17d957489b4c935ab61683a741e40c08
Adam Szamalek <szamalekadam@gmail.com>
2025-11-04 21:07:01 +0100

    feat: add initializator for repositories and services

--- src/shared/config/startup.rs
+++ src/shared/config/startup.rs
@@ -1,5 +1,11 @@
-use crate::infrastructure::persistence::database;
+use crate::{
+    application::services::AuthService,
+    domain::repositories::UserRepository,
+    infrastructure::persistence::{SeaOrmUserRepository, database},
+    shared::config::AppState,
+};
 use sea_orm::DatabaseConnection;
+use std::sync::Arc;
 use tracing::info;
 
 pub async fn initialize_infrastructure() -> Result<DatabaseConnection, Box<dyn std::error::Error>> {
@@ -10,4 +16,21 @@ pub async fn initialize_infrastructure() -> Result<DatabaseConnection, Box<dyn s
     info!("Successfully connected to the PostgreSQL database");
 
     Ok(database)
+}
+
+pub fn initialize_repositories(database: DatabaseConnection) -> Arc<dyn UserRepository> {
+    let user_repository =
+        Arc::new(SeaOrmUserRepository::new(database.clone())) as Arc<dyn UserRepository>;
+
+    info!("Successfully initialized repositories");
+
+    user_repository
+}
+
+pub fn initialize_services(user_repository: Arc<dyn UserRepository>) -> AppState {
+    let auth_service = Arc::new(AuthService::new(user_repository.clone()));
+
+    info!("Successfully initialized services");
+
+    AppState::new(auth_service)
 }
