fa392864d96001eef09e941c97b9ed02cafcd1c8
Adam Szamalek <szamalekadam@gmail.com>
2025-11-05 21:10:08 +0100

    feat: add ActivationQueryDto

--- src/application/dto/auth_dto.rs
+++ src/application/dto/auth_dto.rs
@@ -2,9 +2,17 @@ use serde::{Deserialize, Serialize};
 use validator::Validate;
 
 #[derive(Debug, Clone, Serialize, Deserialize, Validate)]
+#[serde(rename_all = "camelCase")]
 pub struct LoginDto {
     #[validate(email(message = "Invalid email format"))]
     pub email: String,
     #[validate(length(min = 1, message = "Password is required"))]
     pub password: String,
+}
+
+#[derive(Debug, Clone, Deserialize, Serialize, Validate)]
+#[serde(rename_all = "camelCase")]
+pub struct ActivationQueryDto {
+    #[validate(length(min = 1, message = "Activation token is required"))]
+    pub activation_token: String,
 }
--- src/application/dto/mod.rs
+++ src/application/dto/mod.rs
@@ -1,5 +1,5 @@
 pub mod auth_dto;
 pub mod user_dto;
 
-pub use auth_dto::LoginDto;
+pub use auth_dto::{ActivationQueryDto, LoginDto};
 pub use user_dto::{CreateUserDto, UserDto};
