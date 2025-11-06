f4b07adfe297ba6e9f617772cc0676ad24b98ddc
Adam Szamalek <szamalekadam@gmail.com>
2025-11-06 15:20:42 +0100

    feat: add forgot and reset password DTOs

--- src/application/dto/mod.rs
+++ src/application/dto/mod.rs
@@ -1,5 +1,8 @@
 pub mod auth_dto;
 pub mod user_dto;
 
-pub use auth_dto::{ActivationQueryDto, LoginDto, ResendActivationQueryDto};
+pub use auth_dto::{
+    ActivationQueryDto, ForgotPasswordQueryDto, LoginDto, ResendActivationQueryDto,
+    ResetPasswordDto,
+};
 pub use user_dto::{CreateUserDto, UserDto};
