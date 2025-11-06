8c5084dcea395d0370f78b30cbe9834d0a90c815
Adam Szamalek <szamalekadam@gmail.com>
2025-11-06 18:17:47 +0100

    feat: add user controller and profile endpoint

--- src/presentation/http/mod.rs
+++ src/presentation/http/mod.rs
@@ -1,5 +1,7 @@
 pub mod auth_controller;
 pub mod server;
+pub mod user_controller;
 
 pub use auth_controller::configure as configure_auth_roures;
 pub use server::configure_server;
+pub use user_controller::configure as configure_user_routes;
