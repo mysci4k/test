6028711a91bf41a3286b2b65579f4001314fb69d
Adam Szamalek <szamalekadam@gmail.com>
2025-11-08 11:38:04 +0100

    feat: add board DTOs

--- src/application/dto/mod.rs
+++ src/application/dto/mod.rs
@@ -1,8 +1,10 @@
 pub mod auth_dto;
+pub mod board_dto;
 pub mod user_dto;
 
 pub use auth_dto::{
     ActivationQueryDto, ForgotPasswordQueryDto, LoginDto, ResendActivationQueryDto,
     ResetPasswordDto,
 };
+pub use board_dto::{BoardDto, CreateBoardDto};
 pub use user_dto::{CreateUserDto, UserDto};
