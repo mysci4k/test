e0bb9c9df9571d5f9c4afaa9466512592b749b1f
Adam Szamalek <szamalekadam@gmail.com>
2025-11-08 11:42:19 +0100

    feat: add BoardService to AppState

--- src/shared/config/app_state.rs
+++ src/shared/config/app_state.rs
@@ -1,17 +1,23 @@
-use crate::application::services::{AuthService, UserService};
+use crate::application::services::{AuthService, BoardService, UserService};
 use std::sync::Arc;
 
 #[derive(Clone)]
 pub struct AppState {
     pub auth_service: Arc<AuthService>,
     pub user_service: Arc<UserService>,
+    pub board_service: Arc<BoardService>,
 }
 
 impl AppState {
-    pub fn new(auth_service: Arc<AuthService>, user_service: Arc<UserService>) -> Self {
+    pub fn new(
+        auth_service: Arc<AuthService>,
+        user_service: Arc<UserService>,
+        board_service: Arc<BoardService>,
+    ) -> Self {
         Self {
             auth_service,
             user_service,
+            board_service,
         }
     }
 }
