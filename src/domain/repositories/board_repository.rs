5e9109a6ea5cd51f158ea6f986dabba3cf7d3312
Adam Szamalek <szamalekadam@gmail.com>
2025-11-08 11:38:47 +0100

    feat: add Board repository trait and model

---
+++ src/domain/repositories/board_repository.rs
@@ -0,0 +1,34 @@
+use crate::shared::error::ApplicationError;
+use async_trait::async_trait;
+use chrono::{DateTime, FixedOffset, Utc};
+use uuid::Uuid;
+
+#[derive(Debug, Clone)]
+pub struct Board {
+    pub id: Uuid,
+    pub name: String,
+    pub description: Option<String>,
+    pub owner_id: Uuid,
+    pub created_at: DateTime<FixedOffset>,
+    pub updated_at: DateTime<FixedOffset>,
+}
+
+impl Board {
+    pub fn new(id: Uuid, name: String, description: Option<String>, owner_id: Uuid) -> Self {
+        let now = Utc::now().fixed_offset();
+
+        Self {
+            id,
+            name,
+            description,
+            owner_id,
+            created_at: now,
+            updated_at: now,
+        }
+    }
+}
+
+#[async_trait]
+pub trait BoardRepository: Send + Sync {
+    async fn create(&self, board: Board) -> Result<Board, ApplicationError>;
+}
--- src/domain/repositories/mod.rs
+++ src/domain/repositories/mod.rs
@@ -1,3 +1,5 @@
+pub mod board_repository;
 pub mod user_repository;
 
+pub use board_repository::{Board, BoardRepository};
 pub use user_repository::{User, UserRepository};
