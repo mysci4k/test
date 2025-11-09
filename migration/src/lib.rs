9a20d2064e34530811aa46f7949073a2b063695e
Adam Szamalek <szamalekadam@gmail.com>
2025-11-09 17:08:07 +0100

    feat: add board member migration to Migrator

--- migration/src/lib.rs
+++ migration/src/lib.rs
@@ -2,6 +2,7 @@ mod m20251102_200527_create_user_table;
 mod m20251102_201124_create_board_table;
 mod m20251102_201821_create_column_table;
 mod m20251102_202640_create_task_table;
+mod m20251108_111856_create_board_member_table;
 
 pub use sea_orm_migration::prelude::*;
 
@@ -15,6 +16,7 @@ impl MigratorTrait for Migrator {
             Box::new(m20251102_201124_create_board_table::Migration),
             Box::new(m20251102_201821_create_column_table::Migration),
             Box::new(m20251102_202640_create_task_table::Migration),
+            Box::new(m20251108_111856_create_board_member_table::Migration),
         ]
     }
 }
