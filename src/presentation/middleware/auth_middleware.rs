38f642ee11421f64f39db47f03c932c45df00db9
Adam Szamalek <szamalekadam@gmail.com>
2025-11-12 20:52:57 +0100

    feat: allow /scalar paths to bypass auth middleware

--- src/presentation/middleware/auth_middleware.rs
+++ src/presentation/middleware/auth_middleware.rs
@@ -51,7 +51,10 @@ where
         Box::pin(async move {
             let path = req.path().to_string();
 
-            if path == "/api/" || (path.starts_with("/api/auth") && path != "/api/auth/logout") {
+            if path == "/api/"
+                || (path.starts_with("/api/auth") && path != "/api/auth/logout")
+                || path.starts_with("/scalar")
+            {
                 return service.call(req).await;
             }
 
