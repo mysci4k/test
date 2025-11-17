bf7723e03f577a6dd5427b5d6a679ee12ccdade0
Adam Szamalek <szamalekadam@gmail.com>
2025-11-17 20:14:20 +0100

    feat: add add_board_member method to BoardService

--- src/application/services/board_service.rs
+++ src/application/services/board_service.rs
@@ -1,25 +1,32 @@
 use crate::{
-    application::dto::{BoardDto, CreateBoardDto, UpdateBoardDto},
-    domain::repositories::{Board, BoardMember, BoardMemberRepository, BoardRepository},
+    application::dto::{
+        AddBoardMemberDto, BoardDto, BoardMemberDto, CreateBoardDto, UpdateBoardDto,
+    },
+    domain::repositories::{
+        Board, BoardMember, BoardMemberRepository, BoardRepository, UserRepository,
+    },
     shared::error::ApplicationError,
 };
 use chrono::Utc;
-use entity::{BoardMemberRoleEnum, BoardModel};
+use entity::{BoardMemberModel, BoardMemberRoleEnum, BoardModel};
 use std::sync::Arc;
 use uuid::Uuid;
 use validator::Validate;
 
 pub struct BoardService {
+    user_repository: Arc<dyn UserRepository>,
     board_repository: Arc<dyn BoardRepository>,
     board_member_repository: Arc<dyn BoardMemberRepository>,
 }
 
 impl BoardService {
     pub fn new(
+        user_repository: Arc<dyn UserRepository>,
         board_repository: Arc<dyn BoardRepository>,
         board_member_repository: Arc<dyn BoardMemberRepository>,
     ) -> Self {
         Self {
+            user_repository,
             board_repository,
             board_member_repository,
         }
@@ -145,6 +152,52 @@ impl BoardService {
             owner_id: updated_board.owner_id,
             created_at: updated_board.created_at,
             updated_at: updated_board.updated_at,
+        }))
+    }
+
+    pub async fn add_board_member(
+        &self,
+        dto: AddBoardMemberDto,
+        user_id: Uuid,
+    ) -> Result<BoardMemberDto, ApplicationError> {
+        dto.validate()?;
+
+        if !self
+            .board_member_repository
+            .check_permissions(
+                dto.board_id,
+                user_id,
+                vec![BoardMemberRoleEnum::Owner, BoardMemberRoleEnum::Moderator],
+            )
+            .await?
+        {
+            return Err(ApplicationError::Forbidden {
+                message: "You don't have permission to perform this action".to_string(),
+            });
+        }
+
+        if !self.user_repository.exists_by_id(dto.user_id).await? {
+            return Err(ApplicationError::NotFound {
+                message: "User with the given ID not found".to_string(),
+            });
+        }
+
+        let board_member = BoardMember::new(
+            Uuid::now_v7(),
+            dto.board_id,
+            dto.user_id,
+            BoardMemberRoleEnum::Member,
+        );
+
+        let saved_board_member = self.board_member_repository.create(board_member).await?;
+
+        Ok(BoardMemberDto::from_entity(BoardMemberModel {
+            id: saved_board_member.id,
+            board_id: saved_board_member.board_id,
+            user_id: saved_board_member.user_id,
+            role: saved_board_member.role,
+            created_at: saved_board_member.created_at,
+            updated_at: saved_board_member.updated_at,
         }))
     }
 }
