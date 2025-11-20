4d1f63d14dc0c1df8c0c5d1b5d230ef45964d67d
Adam Szamalek <szamalekadam@gmail.com>
2025-11-20 19:01:12 +0100

    feat: add WebSocket controller and exports

--- src/presentation/http/mod.rs
+++ src/presentation/http/mod.rs
@@ -3,9 +3,11 @@ pub mod board_controller;
 pub mod openapi;
 pub mod server;
 pub mod user_controller;
+pub mod websocket_controller;
 
 pub use auth_controller::configure as configure_auth_roures;
 pub use board_controller::configure as configure_board_routes;
 pub use openapi::ApiDoc;
 pub use server::configure_server;
 pub use user_controller::configure as configure_user_routes;
+pub use websocket_controller::configure as configure_websocket_routes;
