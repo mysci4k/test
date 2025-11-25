2b3e7bfbf013f18197217a252b6a112128b5a3e9
Adam Szamalek <szamalekadam@gmail.com>
2025-11-25 13:31:20 +0100

    feat: export column DTOs

--- src/application/dto/mod.rs
+++ src/application/dto/mod.rs
@@ -1,6 +1,7 @@
 pub mod auth_dto;
 pub mod board_dto;
 pub mod board_member_dto;
+pub mod column_dto;
 pub mod user_dto;
 
 pub use auth_dto::{
@@ -11,4 +12,5 @@ pub use board_dto::{BoardDto, CreateBoardDto, UpdateBoardDto};
 pub use board_member_dto::{
     AddBoardMemberDto, BoardMemberDto, DeleteBoardMemberDto, UpdateBoardMemberRoleDto,
 };
+pub use column_dto::{ColumnDto, CreateColumnDto};
 pub use user_dto::{CreateUserDto, UserDto};
