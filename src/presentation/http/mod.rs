9301740e2c3edbfe0341975d712e4cbdbb713da6
Adam Szamalek <szamalekadam@gmail.com>
2025-11-25 13:33:47 +0100

    feat: add column controller and export routes

--- src/presentation/http/mod.rs
+++ src/presentation/http/mod.rs
@@ -1,5 +1,6 @@
 pub mod auth_controller;
 pub mod board_controller;
+pub mod column_controller;
 pub mod openapi;
 pub mod server;
 pub mod user_controller;
@@ -7,6 +8,7 @@ pub mod websocket_controller;
 
 pub use auth_controller::configure as configure_auth_roures;
 pub use board_controller::configure as configure_board_routes;
+pub use column_controller::configure as configure_column_routes;
 pub use openapi::ApiDoc;
 pub use server::configure_server;
 pub use user_controller::configure as configure_user_routes;
--- src/presentation/mod.rs
+++ src/presentation/mod.rs
@@ -3,5 +3,6 @@ pub mod middleware;
 
 pub use http::configure_auth_roures;
 pub use http::configure_board_routes;
+pub use http::configure_column_routes;
 pub use http::configure_user_routes;
 pub use http::configure_websocket_routes;
