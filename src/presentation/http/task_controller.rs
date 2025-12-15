213ca66ee951332d3e934fb4749e6700fff562f3
Adam Szamalek <szamalekadam@gmail.com>
2025-12-15 18:27:13 +0100

    feat: add get_task endpoint

--- src/presentation/http/task_controller.rs
+++ src/presentation/http/task_controller.rs
@@ -8,12 +8,12 @@ use crate::{
         response::{ApiResponse, ApiResponseSchema},
     },
 };
-use actix_web::{post, web};
+use actix_web::{get, post, web};
 use std::sync::Arc;
 use uuid::Uuid;
 
 pub fn configure(cfg: &mut web::ServiceConfig) {
-    cfg.service(web::scope("/task").service(create_task));
+    cfg.service(web::scope("/task").service(create_task).service(get_task));
 }
 
 #[utoipa::path(
@@ -46,5 +46,42 @@ async fn create_task(
     Ok(ApiResponse::Created {
         message: "Task created successfully".to_string(),
         data: task,
+    })
+}
+
+#[utoipa::path(
+    get,
+    description = "***PROTECTED ENDPOINT***\n\nRetrieves detailed information about a specific task by its ID. User must be a member of the board to access this endpoint.",
+    path = "/task/{taskId}",
+    params(
+        ("taskId" = Uuid, Path, description = "Unique identifier of the task")
+    ),
+    responses(
+        (status = 200, description = "OK - Task data retrieved successfully", body = ApiResponseSchema<TaskDto>),
+        (status = 401, description = "Unauthorized - No active session or session has expired", body = ApplicationErrorSchema),
+        (status = 403, description = "Forbidden - User doesn't have access to this board", body = ApplicationErrorSchema),
+        (status = 404, description = "Not Found - Task with the given ID not found", body = ApplicationErrorSchema),
+        (status = 500, description = "Internal Server Error - Failed to retrieve task", body = ApplicationErrorSchema)
+    ),
+    tag = "Task",
+    security(
+        ("session_cookie" = [])
+    )
+)]
+#[get("/{taskId}")]
+async fn get_task(
+    task_service: web::Data<Arc<TaskService>>,
+    task_id: web::Path<Uuid>,
+    user_id: web::ReqData<Uuid>,
+) -> Result<ApiResponse<TaskDto>, ApplicationError> {
+    let task_id = task_id.into_inner();
+    let user_id = user_id.into_inner();
+    let task = task_service.get_task_by_id(task_id, user_id).await?;
+
+    Ok(ApiResponse::Found {
+        message: "Task data retrieved successfully".to_string(),
+        data: task,
+        page: None,
+        total_pages: None,
     })
 }
